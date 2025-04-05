bf20e5dbbd6d5f0acbd889336ec7740d85cf4206
Pascal Feigenwinter <pfeigenwi@student.ethz.ch>
2025-04-05 21:39:49 +0200

    alex

--- eval_job.sh
+++ eval_job.sh
@@ -2,10 +2,9 @@
 
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=48
-#SBATCH --time=0:40:00
+#SBATCH --time=0:50:00
 #SBATCH --job-name="model evaluation"
+#SBATCH --output=/cluster/home/pfeigenwi/OpenDeepSearch/sbatch.out
 #SBATCH --mem-per-cpu=2048
-#SBATCH --output="./evals/datasets/frames_test_set.csv"
-#SBATCH --open-mode=append
 
- python ./evals/eval_tasks.py --parallel-workers=48 --num-trials=1 --eval-tasks=./evals/datasets/frames_test_set.csv
+python  $HOME/OpenDeepSearch/evals/eval_tasks.py --parallel-workers=48 --num-trials=1 --eval-tasks=$HOME/OpenDeepSearch/evals/datasets/frames_test_set.csv
